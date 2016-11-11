ba85509a5f568e844685074a9b0daeb830cc588a
Scarecro <scarecro.design@gmail.com>
2016-11-11 18:37:16 -0500

    Index Added
    Index.php has been created

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!exercise 2>
+<html>
+  <head>
+    <title>Definitions</title>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="main.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <input type ="text" id="query"/>
+    <input type ="submit" value="Search" id="search"/>
+    
+    <p id="result"></p>
+  </body>
+</html>
