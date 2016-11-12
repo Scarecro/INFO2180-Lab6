f174f63acf6e8bbd9c54e57ea8d119bcfd71b509
Scarecro <scarecro.design@gmail.com>
2016-11-11 19:51:18 -0500

    Added List All
    Added functions to list all definitions in an OL via xml

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 $(function(){
-    var search = $("#search");
-    var searchAll = $("#searchAll");
-    var result = $("#result");
-    
-    search.on("click", function(element){
-        console.log('searching...');
-      
-        //element.preventDefault();
-        var word = $("input").val();
-        if (word != "") {
-            $.ajax("request.php?q=" + word, {
-                method: "GET"
-            }).done(function(response) {
-                console.log('done');
-                alert(response);
-                $(result).html(response);
-            }).fail(function() {
-                console.log('fail');
-                $(result).html("There was a problem with your request. ");
-            });   
-        }
-        else { 
-          $(result).html("Please enter a word. ");
-            
-        }
-            
-    });
+  var search = $("#search");
+  var listAll = $("#listall");
+  var result = $("#result");
+
+  search.on("click", function(){
+    console.log('searching...');
+
+    var word = $("input").val();
+    if (word != "") {
+      $.ajax("request.php?q=" + word, {
+          method: "GET"
+      }).done(function(response) {
+          console.log('done');
+          alert(response);
+          $(result).html(response);
+      }).fail(function() {
+          console.log('fail');
+          $(result).html("There was a problem with your request. ");
+      });   
+    }
+    else { 
+      $(result).html("Please enter a word. ");
+
+    }
+  });
   
+  listAll.on("click", function(){
+    console.log('listing all...');
+    var output = '<ol>';
+    
+    $.ajax("request.php?all=true", {
+      method: "GET"
+    }).done(function(response) {
+      console.log('done');
+      alert(response);
+
+      var myxml = $.parseXML(response);
+      $(myxml).find('definition').each(function (index, item) {
+        output += '<li><strong>'+ $(this).attr('name') +'</strong> - '+ $(this).text() +'</li>';
+      });
+      $(result).html(output);
+    }).fail(function() {
+      console.log('fail');
+      $(result).html("There was a problem with your request. ");
+    }); 
+  });
+
 });
-    
-    
+
+
 
--- request.php
+++ request.php
@@ -1,18 +1,37 @@
 <?php
 
-// accept a term (keyword)
-// respond with a value
+  // accept a term (keyword)
+  // respond with a value
 
-$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+  if(isset($_GET['q']))
+    $query = $_GET['q'];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+  $definition = [
+      "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+      "bar" => "A place that sells alcholic beverages",
+      "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+      "html" => "The standard markup language for creating web pages and web applications.",
+      "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+      "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+      "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+  ];
+  if(isset($_GET['all']) && $_GET['all']==true){
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<entries>';
+
+    foreach($definition as $key => $val){
+      $xml .='<definition name="'.$key.'" author="author">';
+      $xml .= $val;
+      $xml .= '</definition>';
+    }
+
+    $xml .= '</entries>';
+    print $xml;
+  }
+  else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+  }
+
+
+?>
